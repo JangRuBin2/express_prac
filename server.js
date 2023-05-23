0d5976b5dd5be1b63be4094ad57ac83ea2e3934f
jangrubin <wkdfnqls2465@gmail.com>
2023-05-23 15:25:56 +0900

    미들웨어 설정 및 express 포트 지정

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
 import express from "express";
-console.log(express);
+// console.log(express);
+
+const app = express();
+const port = 3000;
+
+// ! 미들 웨어 설정
+// json 형식의 요청 바디 파싱
+app.use(express.json());
+// 인코딩 된 요청 바디 파싱
+app.use(express.urlencoded({ extended : true }));
