6773052bdb45a5cde3292124416613bf6fb1b4b4
jangrubin2 <wkdfnqls2465@gmail.com>
2023-05-28 14:29:30 +0900

    태초 마을에서 express 서버 실행

--- server.js
+++ server.js
@@ -3,9 +3,14 @@ import express from "express";
 
 const app = express();
 const port = 3000;
-
+app.get("/", (req, res)=> {
+    res.send("Hello World");
+});
+app.listen(8080, ()=> {
+console.log("express server is started");
+})
 // ! 미들 웨어 설정
 // json 형식의 요청 바디 파싱
-app.use(express.json());
+// app.use(express.static("/"));
 // 인코딩 된 요청 바디 파싱
-app.use(express.urlencoded({ extended : true }));
+// app.use(express.urlencoded({ extended : true }));
